4f4a8f4e1be9cb55196ab6067ec11736d9f7a37f
Yashwant Kumar <53390920+itsyashh@users.noreply.github.com>
2020-08-01 07:13:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ak13_alpha_keens
+ AAI-SIH-2020-AK13-Solution_Team_Alpha_Keens
---
+++ android/app/src/main/kotlin/com/example/alpha_keens/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.alpha_keens
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
